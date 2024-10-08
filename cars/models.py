988e355e9f0e59e977b3819f9b1eb9e71717957b
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-10-08 10:47:13 -0300

    updated licence_plate lenght

--- cars/models.py
+++ cars/models.py
@@ -5,7 +5,7 @@ class Car(models.Model):
     full_name = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=20)
     team = models.CharField(max_length=100)
-    car_identification = models.CharField(max_length=7, unique=True)
+    car_identification = models.CharField(max_length=10 unique=True)
     car_model = models.CharField(max_length=200)
     cars_per_team = models.IntegerField()
 
