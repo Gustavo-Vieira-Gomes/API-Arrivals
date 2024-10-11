8258e4f79fa5c4aee4e63bc224756ac0e531125d
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-10-11 16:00:55 -0300

    format start time

--- starts/serializers.py
+++ starts/serializers.py
@@ -7,3 +7,6 @@ class StartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Start
         fields = '__all__'
+
+    def get_start_time(self, obj):
+        return obj.start_time.strftime('%d/%m%Y %H:%M%S')
